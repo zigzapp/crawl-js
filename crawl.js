502c9155b3a33c0753cece881af60687025d33a8
magnus <not@me.com>
2024-02-20 09:34:37 +0100

    getURLelements from html tests

--- crawl.js
+++ crawl.js
@@ -1,5 +1,35 @@
+const { JSDOM } = require('jsdom')
+
 module.exports  = {
-    normalizeURL
+    normalizeURL,
+    getURLsFromHTML
+}
+
+function getURLsFromHTML(htmlBody, baseURL){
+    const urls = []
+    const dom = new JSDOM(htmlBody)
+    const linkElements = dom.window.document.querySelectorAll('a')
+    for (const linkElement of linkElements) {
+        // console.log(linkElement.href) // remove after debug
+        if (linkElement.href.slice(0, 1) === '/') {
+            //relative
+            try {
+                const urlObj = new URL(`${baseURL}${linkElement.href}`)
+                urls.push(urlObj.href)
+            } catch (err){
+                console.error(`error with relative url: ${err.message}`)
+            }
+        } else {
+            //absolue
+            try {
+                const urlObj = new URL(linkElement.href)
+                urls.push(urlObj.href)
+            } catch (err){
+                console.error(`error with absolute url: ${err.message}`)
+            }
+        }
+    }
+    return urls
 }
 
 function normalizeURL(url) {
